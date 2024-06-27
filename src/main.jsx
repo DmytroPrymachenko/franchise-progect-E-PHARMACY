57aa177e3a06654c5f0b40cf44933be0dbfe8199
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2024-06-27 14:33:49 +0300

    App+ main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import { ToastContainer } from "react-toastify";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/franchise-progect-E-PHARMACY">
+    {/* <Provider store={store}> */}
+    {/* <PersistGate loading={null} persistor={persistor}> */}
     <App />
-  </React.StrictMode>,
-)
+    <ToastContainer autoClose={1500} />
+    {/* </PersistGate> */}
+    {/* </Provider> */}
+  </BrowserRouter>
+);
