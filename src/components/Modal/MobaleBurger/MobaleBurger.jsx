eeb54d7674616e280607819d7d23a445d827044c
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2024-06-27 23:56:23 +0300

    burger css

--- src/components/Modal/MobaleBurger/MobaleBurger.jsx
+++ src/components/Modal/MobaleBurger/MobaleBurger.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Backdrop from "../../Backdrop/Backdrop";
 import "./MobaleBurger.Styled.css";
+import ExitSVG from "../../../images/svg/Exit/ExitSVG";
 
 const MobaleBurger = ({ isOpen, closeModal }) => {
   return (
@@ -8,10 +9,22 @@ const MobaleBurger = ({ isOpen, closeModal }) => {
       <Backdrop closeModal={closeModal} isOpen={isOpen} />
       <div className={`modal-wrapper ${isOpen ? "open" : ""}`}>
         <>
-          <div className="burger">
-            <Link>Home</Link>
-            <Link>Medicine store</Link>
-            <Link>Medicine</Link>
+          <div className="burger ">
+            <div className="burger visible-increase">
+              <Link className="burger__link first-adapter visible-increase">
+                Home
+              </Link>
+              <Link className="burger__link second-adapter visible-increase">
+                Medicine store
+              </Link>
+              <Link className="burger__link visible-increase">Medicine</Link>
+            </div>
+            <button
+              onClick={closeModal}
+              className="modal__exit visible-increase"
+            >
+              <ExitSVG />
+            </button>
           </div>
         </>
       </div>
